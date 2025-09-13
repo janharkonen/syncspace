0d0edaf1294a823cf6a4746c3882e8d75b2efe5e
Jan Härkönen <jan.harkonen@gmail.com>
2025-09-13 22:04:11 +0300

    add "add workspace" button

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import {
-  useQuery,
-} from "convex/react";
+import { useQuery, useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { useRouter, useParams } from "next/navigation";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogTrigger, DialogClose } from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function Sidebar({className}: {className: string}) {
   const { workspaceEntries } = useQuery(api.workspaceFunctions.workspaceItems) ?? { workspaceEntries: [] };
@@ -13,32 +15,81 @@ export default function Sidebar({className}: {className: string}) {
   return (
     <>
     <div className={cn("text-white h-full sidebar bg-[var(--header-background)] overflow-y-auto border-r-1 border-header-border", className)}>
-        <div>
-            <div>
-                {workspaceEntries.map((workspaceEntry) => (
-                    <Button
-                    key={workspaceEntry._id}
-                        className="
-                            w-full 
-                            cursor-pointer 
-                            overflow-hidden 
-                            text-ellipsis 
-                            whitespace-nowrap
-                            text-left
-                            justify-start
-                        "
-                        variant={workspaceId === workspaceEntry._id ? "sidebarbuttonactive" : "sidebarbutton"}
-                        onClick={() => { 
-                            router.push(`/workspaces/${workspaceEntry._id}`);
-                        }}
-                        title={workspaceEntry.workspacename}
-                        >
-                        {workspaceEntry.workspacename}
-                    </Button>
-                ))}
-            </div>
-        </div>
+        <DialogCloseButton />
+        {workspaceEntries.map((workspaceEntry) => (
+            <Button
+            key={workspaceEntry._id}
+                className="
+                    w-full 
+                    cursor-pointer 
+                    overflow-hidden 
+                    text-ellipsis 
+                    whitespace-nowrap
+                    text-left
+                    justify-start
+                "
+                variant={workspaceId === workspaceEntry._id ? "sidebarbuttonactive" : "sidebarbutton"}
+                onClick={() => { 
+                    router.push(`/workspaces/${workspaceEntry._id}`);
+                }}
+                title={workspaceEntry.workspacename}
+                >
+                {workspaceEntry.workspacename}
+            </Button>
+        ))}
     </div>
     </>
   )
 }
+
+function DialogCloseButton( ) {
+    const [workspaceName, setWorkspaceName] = useState("");
+    const createWorkspace = useMutation(api.workspaceFunctions.createWorkspace);
+    const router = useRouter();
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+            <div className="p-2">
+              <Button className="w-full hover:cursor-pointer">+ Add workspace</Button>
+            </div>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>New workspace</DialogTitle>
+            <DialogDescription>
+              Give a name to your new workspace.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex items-center gap-2">
+            <div className="grid flex-1 gap-2">
+              <Label htmlFor="link" className="sr-only">
+                Link
+              </Label>
+              <Input
+                id="link"
+                onChange={(e) => {
+                  setWorkspaceName(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <DialogFooter className="sm:justify-start">
+            <DialogClose asChild>
+              <Button type="button" variant="outline" className="hover:cursor-pointer"
+              onClick={() => {
+                const workspaceId = createWorkspace({ workspaceName: workspaceName });
+                workspaceId.then((workspaceId) => {
+                    if (workspaceId) {
+                     router.push(`/workspaces/${workspaceId}`);
+                    }
+                });
+              }}
+              >
+                OK
+              </Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    )
+  }
--- convex/workspaceFunctions.ts
+++ convex/workspaceFunctions.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 // Write your Convex functions in any file inside this directory (`convex`).
 // See https://docs.convex.dev/functions for more.
@@ -12,13 +12,39 @@ export const workspaceItems = query({
     //// See https://docs.convex.dev/database/reading-data.
     
     const user = await ctx.auth.getUserIdentity();
-    console.log(user);
+    if (!user) {
+      throw new Error("User not authenticated");
+    }
+    const userId = user.tokenIdentifier.split("|")[1];
     const workspaceEntries = await ctx.db
       .query("workspace_list")
+      .filter((q) => q.eq(q.field("userId"), userId))
       .order("desc")
       .collect();
     return {
       workspaceEntries: workspaceEntries,
     };
+  },
+});
+
+export const createWorkspace = mutation({
+
+  args: {
+    workspaceName: v.string(),
+  },
+
+  handler: async (ctx, args) => {
+    //// Read the database as many times as you need here.
+    //// See https://docs.convex.dev/database/reading-data.
+    const user = await ctx.auth.getUserIdentity();
+    if (!user) {
+      throw new Error("User not authenticated");
+    }
+    const userId = user.tokenIdentifier.split("|")[1];
+    const workspaceId = await ctx.db.insert("workspace_list", {
+      workspacename: args.workspaceName,
+      userId: userId,
+    });
+    return workspaceId;
   },
 });
